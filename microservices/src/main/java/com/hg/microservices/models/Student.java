4d132b4b85b66878de82ca5019db9af786ba1c7b
Isa Gonzalez <isabelmariagonzalezrodriguez@gmail.com>
2022-11-15 14:36:39 +0100

    Nuevo ejercicio: cambios

--- microservices/src/main/java/com/hg/microservices/models/Student.java
+++ microservices/src/main/java/com/hg/microservices/models/Student.java
@@ -1,5 +1,6 @@
 package com.hg.microservices.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serializable;
@@ -7,4 +8,8 @@ import java.io.Serializable;
 @Entity(name = "Student")
 @Table(name = "\"Students\"", schema = "public")
 public class Student extends Person implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Column(name = "schoolarship", columnDefinition = "boolean DEFAULT 'false'")
+	private Boolean schoolarship;
 }
